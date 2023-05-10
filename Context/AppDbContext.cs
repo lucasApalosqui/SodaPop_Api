7137a606011b21a07556143507bcb2bae95110ff
LucasApalosqui <lucas.apalosqui@gmail.com>
2023-05-10 12:45:44 -0300

    [ TASK: add AppDbContext, first configures and DbSet user model ]

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SodaPop.Models;
+
+namespace SodaPop.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Tbl_User { get; set; }
+    }
+}
