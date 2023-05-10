c4a7675d61e934309da0cd68600a8a5211b09e0b
LucasApalosqui <lucas.apalosqui@gmail.com>
2023-05-10 14:14:24 -0300

    [ TASK: Add Db string connection and make first migration ]

---
+++ Migrations/20230510170834_first_migration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SodaPop.Migrations
+{
+    public partial class first_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tbl_User",
+                columns: table => new
+                {
+                    id_user = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    title_user = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    description_user = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    avatar_user = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    banner_user = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    average_user_score = table.Column<double>(type: "float", nullable: false),
+                    favorite_piece = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    worst_piece = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    hated_character = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true),
+                    lovely_character = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tbl_User", x => x.id_user);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tbl_User");
+        }
+    }
+}
